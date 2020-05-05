dd92d72ac5c3ca6049a33a881017fc5223251a6e
RobertBeekelaar <64256219+RobertBeekelaar@users.noreply.github.com>
2020-05-05 14:59:53 +0200

    Add files via upload

---
+++ W1D2-4IfStatements/script2.js
@@ -0,0 +1,34 @@
+const myAge = 18;
+const isFemale = false;
+const driverStatus = 'bob';
+
+if (myAge >= 18) {
+
+  console.log('Welcome at Wasted!');
+
+} else {
+
+  console.log('Sorry you are not old enough to enter Wasted!');
+
+}
+
+
+if (isFemale) {
+
+  console.log('Get ready for Ladiesnight!');
+
+} else {
+
+  console.log('Sorry this is a ladiesnight, see you another time!');
+
+}
+
+if (driverStatus == 'bob') {
+
+  console.log('Well done bob, you can drive home safely!');
+
+} else {
+
+  console.log('Let someone else drive, or take a cab!');
+
+}
