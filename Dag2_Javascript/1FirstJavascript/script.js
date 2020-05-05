4cdf81645af57c1bc1ecb0dcb9e43039e91c186f
RobertBeekelaar <64256219+RobertBeekelaar@users.noreply.github.com>
2020-05-05 10:17:28 +0200

    Add files via upload

---
+++ Dag2_Javascript/1FirstJavascript/script.js
@@ -0,0 +1,21 @@
+
+console.log('Hello WINC Academy');
+let firstName = 'Robert ';
+let lastName = 'Beekelaar';
+
+console.log(firstName + lastName);
+
+let age = 31;
+let date = 2020;
+let daysYear = 365.25;
+let averageAge = 76;
+
+console.log('Birthdate:', date - age);
+console.log('Days Alive:', age * daysYear);
+console.log('Devision:', averageAge / age);
+console.log('Module:', averageAge % age);
+console.log(typeof age);
+
+age = 'éénendertig';
+console.log(typeof age);
+
