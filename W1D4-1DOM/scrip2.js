af59d4b4c7d2e456a5556b2d2cf1ec8f4f1ebc5b
RobertBeekelaar <64256219+RobertBeekelaar@users.noreply.github.com>
2020-05-07 12:32:34 +0200

    Add files via upload

---
+++ W1D4-1DOM/scrip2.js
@@ -0,0 +1,11 @@
+//document.body.innerHTML = 'Dit is de nieuwe content van body'
+
+
+document.getElementById('first-section');
+
+let first = document.getElementById('first-section');
+console.log(first);
+
+document.getElementsByClassName('paragraph');
+let paragraphs = document.getElementsByClassName('paragraph');
+console.log(paragraphs);
